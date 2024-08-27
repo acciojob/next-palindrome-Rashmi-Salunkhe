0f910753829930a21496563ad5a9d81f32a2d9a1
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-08-27 14:51:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function nextPalindrome(num) {
-  //your JS code here.
+    // Helper function to check if a number is a palindrome
+    const isPalindrome = (n) => {
+        const str = n.toString();
+        return str === str.split('').reverse().join('');
+    };
+
+    // Start from the next number
+    let nextNum = parseInt(num) + 1;
+
+    // Keep incrementing until we find a palindrome
+    while (!isPalindrome(nextNum)) {
+        nextNum++;
+    }
+
+    return nextNum;
 }
 
-const input = prompt("Enter a palindrome number");
+// Do not change the code below
+const input = prompt("Enter a palindrome number:");
 alert(nextPalindrome(input));
